7b8cba3ea4cb7cbc9bf96409270f6ace26c732f7
tylerd92 <frostykat@protonmail.com>
2021-11-16 00:06:14 -0500

    Create image route

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import routes from './routes/api';
+import images from './routes/api/image';
 
 const app = express();
 const port = 3000;
+
+app.use('/api', routes);
+
+app.use('/api/images', images);
 
 app.listen(port, () => {
   console.log(`listening at port: ${port}`);
